a8c39adf845f6b33748476a1de500beef9f16392
LabManOO1 <maxmaximmax37@gmail.com>
2026-07-08 17:51:41 +0300

    добавил эндпоинты регистрации и авторизации пользователя

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from . import rooms, root
+from . import rooms, root, auth
 
 main_router = APIRouter()
 
 main_router.include_router(rooms.rooms_router)
 main_router.include_router(root.root_router)
+main_router.include_router(auth.auth_router)
