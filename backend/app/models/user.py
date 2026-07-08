dafef4ddc0c2606ed2513fb6c06e49dbfe899cb8
LabManOO1 <maxmaximmax37@gmail.com>
2026-07-08 16:02:59 +0300

    Добавил модель таблицы users

---
+++ backend/app/models/user.py
@@ -0,0 +1,12 @@
+import uuid
+from sqlalchemy import Column, String, DateTime, UUID
+from sqlalchemy.sql import func
+from ..database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String(100), unique=True, nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
