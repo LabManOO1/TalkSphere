0a4c467e475bc6e3cba56448b6ae46d1971c229d
Влада Ефремова <vlad4.efremova@yandex.ru>
2026-07-15 16:45:13 +0300

    fix: remove accidentally pushed backend folder

--- backend/app/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from . import rooms, root, auth
-
-rest_router = APIRouter()
-
-rest_router.include_router(rooms.rooms_router)
-rest_router.include_router(root.root_router)
-rest_router.include_router(auth.auth_router)
